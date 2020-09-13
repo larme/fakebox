192c4f56b1243b879900ef2533a450f37540353e
Zhao Shenyang <dev@zsy.im>
2020-09-13 18:28:20 +0800

    osc: simplify phasor

--- fakebox/stdlib/osc.py
+++ fakebox/stdlib/osc.py
@@ -8,23 +8,21 @@ class Phasor(DSPObj):
 
     def __init__(self):
 
-        self.in_n = 4
+        self.in_n = 3
         self.out_n = 1
         self.phase = 0.0
         super().__init__()
 
     def _tick(self, ins):
-        freq = ins[0]
-        init_phase = ins[1]
-        freq_mod = ins[2]
-        reset_phase_gate = ins[3]
+        init_phase = ins[0]
+        freq = ins[1]
+        reset_phase_gate = ins[2]
 
         if reset_phase_gate > 0:
             self.reset_phase(init_phase)
 
         current_phase = self.phase
 
-        freq = freq + freq_mod
         inc_per_sample = freq / self.sample_rate
         raw_phase = current_phase + inc_per_sample
         self.phase = raw_phase % 1
@@ -57,22 +55,21 @@ def make_sinosc():
     return sinosc
 
 
-def make_simple_phasor(freq, init_phase=0.0):
+def make_simple_phasor(init_phase=0.0):
 
-    freq = Const(freq)
     init_phase = Const(init_phase)
     phasor = Phasor()
 
     # 0 freq | 1 init_phase | 2 freq mode in | 3 phase reset in
     # => 0 freq mode in | 1 phase reset in
-    simple_phasor = partial(phasor, [freq, init_phase])
+    simple_phasor = partial(phasor, [init_phase])
 
     return simple_phasor
 
 
-def make_simple_sinosc(freq, init_phase=0.0):
+def make_simple_sinosc(init_phase=0.0):
 
-    phasor = make_simple_phasor(freq, init_phase)
+    phasor = make_simple_phasor(init_phase)
     sinraw = Sin()
     sinosc = Pipe([phasor, sinraw])
     return sinosc
